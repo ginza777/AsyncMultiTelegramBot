876b7cc33b7ebfe843f82cdc47301ca6f02606ae
JeyJey871 <ginza@tuit.uz>
2024-02-21 00:49:44 +0500

    update bot

--- apps/chatgpt_bot/buttons/inline_keyboard.py
+++ apps/chatgpt_bot/buttons/inline_keyboard.py
@@ -50,7 +50,7 @@ def ai_model_setting_keyboard():
     button_list = [{"name": "ChatGPT", "id": 1}, {"name": "GPT-4 Turbo", "id": 2}]
     keyboard = []
     for button in button_list:
-        keyboard.append([InlineKeyboardButton(button["name"], callback_data=f"ai_model_setting_{button["id"]}")])
+        keyboard.append([InlineKeyboardButton(button["name"], callback_data=f"ai_model_setting_{button['id']}")])
     keyboard.append([InlineKeyboardButton("🔙 Back", callback_data=f"setting_back")])
     return InlineKeyboardMarkup(keyboard)
 
@@ -66,7 +66,7 @@ def language_list_keyboard():
     ]
     keyboard = []
     for button in button_list:
-        keyboard.append([InlineKeyboardButton(button["name"], callback_data=f"language_setting_{button["id"]}")])
+        keyboard.append([InlineKeyboardButton(button['name'], callback_data=f"language_setting_{button['id']}")])
     keyboard.append([InlineKeyboardButton("🔙 Back", callback_data=f"setting_back")])
 
     return InlineKeyboardMarkup(keyboard)
