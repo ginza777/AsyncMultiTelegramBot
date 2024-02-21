dee4426065dab017dccdfb25897dee61435b4429
JeyJey871 <ginza@tuit.uz>
2024-02-21 17:22:04 +0500

    update bot

--- restart.sh
+++ restart.sh
@@ -3,9 +3,11 @@
 
 
 # Systemd xizmatlarini qayta boshlash
-sudo systemctl daemon-reload
 
+
+sudo systemctl start bots.socket
+sudo systemctl enable bots.socket
 sudo systemctl restart bots.socket
 sudo systemctl restart bots.service
-
+sudo systemctl daemon-reload
 sudo nginx -t && sudo systemctl restart nginx
