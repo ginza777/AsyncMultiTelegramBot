b701b38a7e38e2d5bc258ddff566f7ac07ba117d
JeyJey871 <ginza@tuit.uz>
2024-02-21 18:10:31 +0500

    update bot

--- apps/chatgpt_bot/bot_functions.py
+++ apps/chatgpt_bot/bot_functions.py
@@ -57,19 +57,19 @@ async def user_balance(update: Update, context: CallbackContext, chat_gpt_user, 
     if count < 10:
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text=f"Your daily limit is not over yet! Please try again tomorrow!\n"
-                 f"Kunlik limit: {count}\n"
-                 f"Foydalangan limit: {count}\n"
-                 f"Qolgan limit: {10-count}\n",
+            text=f"Your daily limit is not over yet! Please try again tomorrow!😀\n"
+                 f"🎃Kunlik limit: {count}\n"
+                 f"🎃Foydalangan limit: {count}\n"
+                 f"🎃Qolgan limit: {10-count}\n",
             parse_mode=ParseMode.HTML,
         )
     else:
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text=f"Sizda kunlik limit tugagan! iltimos ertaga qayta urinib ko'ring!\n"
-                 f"Kunlik limit: {count}\n"
-                 f"Foydalangan limit: {count}\n"
-                 f"Qolgan limit: {10-count}\n",
+            text=f"🔒Sizda kunlik limit tugagan! iltimos ertaga qayta urinib ko'ring!😢😢😢\n"
+                 f"🎈Kunlik limit: {count}\n"
+                 f"🎈Foydalangan limit: {count}\n"
+                 f"🎈Qolgan limit: {10-count}\n",
             parse_mode=ParseMode.HTML,
         )
 
