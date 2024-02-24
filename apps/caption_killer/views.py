4bdf8739dd2547be2222686c482d23acfeb27fdd
JeyJey871 <ginza@tuit.uz>
2024-02-24 18:16:54 +0500

    caption killer bot

--- apps/caption_killer/views.py
+++ apps/caption_killer/views.py
@@ -18,18 +18,30 @@ async def cap_killer(update: Update, context: CallbackContext):
         chat_id = update.channel_post.chat.id
         message_id = update.channel_post.message_id
         caption = update.channel_post.caption or ''
-        new_caption = await filter_caption(caption, chat_id)
+
         # print(new_caption)
         try:
-            if Channel.objects.filter(channel_id=chat_id).exists():
+            status=await exist_checker(chat_id)
+            if status:
+                new_caption = await filter_caption(caption, chat_id)
                 await context.bot.edit_message_caption(chat_id=chat_id, message_id=message_id, caption=new_caption)
             else:
-                message = "adminga qo'shilmagan" + f"""{update.channel_post}"""
+
+                message = "adminga qo'shilmagan" + f"""\n{update.channel_post.sender_chat.title}\n{update.channel_post.sender_chat.type}\n{update.channel_post.sender_chat.id}"""
+                print(message)
                 await send_msg_log(message)
         except Exception as e:
             logger.error(f"Error in start command: {e}")
             message = "caption killer bot\n" + f"Error in start command: {e}"
             await send_msg_log(message)
+
+
+@sync_to_async
+def exist_checker(chat_id):
+    if Channel.objects.filter(channel_id=chat_id).exists():
+        return True
+    else:
+        return False
 
 
 @sync_to_async
@@ -43,7 +55,7 @@ def filter_caption(caption: str, channel_id) -> str:
         print("channel:", channel)
         print(keyword_text_list)
         for key_word in keyword_text_list:
-            caption.replace(key_word, '')
+            caption = caption.replace(key_word, '')  # Fixed here, you need to assign the replaced caption back
         # caption = '\n'.join(line for line in caption.splitlines() if line.strip())
         caption += F"\n\n•┈┈┈•• ✦🍃✦••┈┈┈•\n\n{channel.channel_sign}"
         return caption
